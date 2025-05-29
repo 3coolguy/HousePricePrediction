13540c4ea7d3cebb6214890f6ebdfb89fcb79376
Jacob Helm <jacobisaiahhelm@gmail.com>
2025-05-29 18:22:28 -0400

    EDA

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import the data
+dataset = pd.read_excel("HousePricePrediction.xlsx")
+
+
+# Quick EDA
+#print(dataset.head(5))
+#print(dataset.shape)
